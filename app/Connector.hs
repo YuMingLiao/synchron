5e41ef4193fa44898bffb634eb3345f8e22ffb15
Yu-Ming Liao <dycan0716@gmail.com>
2022-12-13 16:48:37 +0800

    separate Concur and Connector from Main.hs

--- app/Connector.hs
+++ app/Connector.hs
@@ -28,7 +28,7 @@ import           Replica.SVG
 import           Replica.SVG.Props hiding (r)
 import           Replica.Props hiding     (async, loop)
 import           Replica.Events
--- import           Syn
+import           Syn hiding (pool, spawn, orr)
 -- import           Syn.SVG.Replica
 
 import           Var
@@ -44,47 +44,38 @@ import Prelude hiding (div, forever, span)
 
 import Debug.Trace
 import System.IO.Unsafe
-import Concur (runConcur, withPool, step, orr')
-
-counter x = do
-  div [ onClick ] [ text (T.pack $ show x) ]
-  counter (x + 1)
+import Concur (runConcur, withPool, spawn, orr)
 
 
+-- testConnectors :: IO ()
+testConnectors = do
+  HTTP.http 3921 $ \http ->
+    void $ WS.websocket 3922 defaultConnectionOptions $ \wss ->
+    WS.websocket 3923 defaultConnectionOptions $ \wss2 ->
+    Log.logger $ \log -> do
 
-testConcur :: IO ()
-testConcur = Log.logger $ \log -> do
-  v1 <- registerDelay 1000000
-  v2 <- registerDelay 2000000
-  v3 <- registerDelay 1500000
-  v4 <- registerDelay 3000000
-  v5 <- registerDelay 2500000
+      runConcur $ auth http log wss wss2
 
-  (_, rs) <- runConcur $ do
-    (_, rs) <- orr'
-      [ dp log v3 "V3"
-      , dp log v5 "V5"
-      , do
-          (_, rs) <- orr' [ dp log v1 "A", dp log v2 "B", dp log v4 "C" ]
-          (_, rs) <- orr' rs
-          (_, rs) <- orr' rs
-          pure ()
-      ]
-    (_, rs) <- orr' rs
-    orr' rs
+    where
+      auth http log wss wss2 = do
+        r <- HTTP.receive http $ \req respond -> do
+          r <- respond $ responseLBS status200 [] "Hello World"
+          pure (r, "good")
+        log r
+        server log wss wss2
+      
+      server log wss wss2 = withPool $ \pool -> Control.Monad.forever $ do
+        [ws, ws2] <- andd [ WS.accept wss, WS.accept wss2 ]
+        spawn pool (go log ws ws2)
+        
+      go log ws ws2 = do
+        r <- orr
+          [ fmap Left  <$> WS.receive ws
+          , fmap Right <$> WS.receive ws2
+          ]
+        case r of
+          Nothing  -> pure ()
+          _  -> do
+            log $ show r
+            go log ws ws2
 
-  print $ length rs
-
-  where
-    dp log v s = do
-      log ("BEFORE: " <> s)
-      Concur.step $ do
-        v' <- readTVar v
-        check v'
-      log ("AFTER: " <> s)
-
-    f c n = do
-      Concur.step $ writeTChan c (show n)
-      f c (n + 1)
-
-main = testConcur
