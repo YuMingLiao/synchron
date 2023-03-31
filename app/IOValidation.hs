f6808a125ed69efc111d5ec8d57b767c5e1c7b10
Yu-Ming Liao <dycan0716@gmail.com>
2023-03-31 08:50:28 +0800

    Now, fields can validate inputs and show user messages. How about a smart Submit button?

--- app/IOValidation.hs
+++ app/IOValidation.hs
@@ -47,153 +47,9 @@ import System.IO.Unsafe
 import Concur (runConcur, withPool, step)
 import Control.Monad.IO.Class
 
--- testConcur :: IO ()
--- testConcur = Log.logger $ \log -> do
---   v1 <- registerDelay 1000000
---   v2 <- registerDelay 2000000
---   v3 <- registerDelay 1500000
---   v4 <- registerDelay 3000000
---   v5 <- registerDelay 2500000
--- 
---   (_, rs) <- runConcur $ do
---     (_, rs) <- orr'
---       [ dp log v3 "V3"
---       , dp log v5 "V5"
---       , do
---           (_, rs) <- orr' [ dp log v1 "A", dp log v2 "B", dp log v4 "C" ]
---           (_, rs) <- orr' rs
---           (_, rs) <- orr' rs
---           pure ()
---       ]
---     (_, rs) <- orr' rs
---     orr' rs
--- 
---   print $ length rs
--- 
---   where
---     dp log v s = do
---       log ("BEFORE: " <> s)
---       Concur.step $ do
---         v' <- readTVar v
---         check v'
---       log ("AFTER: " <> s)
--- 
---     f c n = do
---       Concur.step $ writeTChan c (show n)
---       f c (n + 1)
--- 
--- testConnectors :: IO ()
--- testConnectors = do
---   HTTP.http 3921 $ \http ->
---     WS.websocket 3922 defaultConnectionOptions $ \wss ->
---     WS.websocket 3923 defaultConnectionOptions $ \wss2 ->
---     Log.logger $ \log -> do
--- 
---       runConcur $ auth http log wss wss2
--- 
---     where
---       auth http log wss wss2 = do
---         r <- HTTP.receive http $ \req respond -> do
---           r <- respond $ responseLBS status200 [] "Hello World"
---           pure (r, "good")
---         log r
---         server log wss wss2
---       
---       server log wss wss2 = withPool $ \pool -> Syn.forever $ do
---         [ws, ws2] <- andd [ WS.accept wss, WS.accept wss2 ]
---         spawn pool (go log ws ws2)
---         
---       go log ws ws2 = do
---         r <- orr
---           [ fmap Left  <$> WS.receive ws
---           , fmap Right <$> WS.receive ws2
---           ]
---         case r of
---           Nothing  -> pure ()
---           _  -> do
---             log $ show r
---             go log ws ws2
--- 
-testWebsockets :: IO (Context () ())
-testWebsockets =
-  WS.websocket 3922 defaultConnectionOptions $ \wss -> do
-  WS.websocket 3923 defaultConnectionOptions $ \wss2 -> run (NodeId 0) $ do
-    (ws, ws2) <- andd (WS.accept wss, WS.accept wss2)
-
-    d <- WS.receive ws
-    d2 <- WS.receive ws2
-
-    WS.send ws d
-    WS.send ws d2
-
-    WS.send ws2 d
-    WS.send ws2 d2
-
-testChat :: IO (Context () ())
-testChat
-  = WS.websocket 3922 defaultConnectionOptions $ \wss ->
-    run (NodeId 0) $
-    pool $ \p ->
-    local $ \msg -> do
-      acceptConn p wss msg
-  where
-    acceptConn p wss msg = do
-      ws <- WS.accept wss
-      spawn p (chatConn ws msg)
-      acceptConn p wss msg
-
-    chatConn ws msg = do
-      r <- orr [ Left <$> WS.receive ws, Right <$> await msg ]
-      case r of
-        Left m -> do
-          emit msg m
-          WS.send ws m
-        Right msg -> WS.send ws msg
-      chatConn ws msg
-
-main = runReplica inputName
-
--- Replica ---------------------------------------------------------------------
-
-data ContainerProps = Click (Event Internal DOMEvent)
-data Container = Label T.Text | Number Int | Container [ContainerProps] [Container]
-
-toHTML (Label x) = HTML $ \_ -> [VText x]
-toHTML (Number x) = HTML $ \_ -> [VText (T.pack $ show x)]
-toHTML (Container props children) = HTML $ \ctx ->
-  [ VNode "div"
-      (M.fromList $ fmap (toProps ctx) props)
-      Nothing
-      (concatMap (($ ctx) . runHTML . toHTML) children)
-  ]
-  where
-    toProps ctx (Click e) = ("onClick", AEvent (EventOptions False False False) $ \de -> void $ push ctx e de)
-
--- abstractConter :: Event Internal Int -> Int -> Syn Container a
--- abstractConter o x = local $ \e -> local $ \f -> do
---   view (Container [Click e] [Number x])
---   await e
---   view (Container [Click f] [Label "You clicked!"])
---   await f
---   emit o (x + 1)
---   abstractConter o (x + 1)
--- 
--- realCounter x = local $ \y -> do
---   void $ andd (mapView toHTML (abstractConter y x), label x y)
---   where
---     label x y = do
---       Right x' <- orr [ Left <$> text (T.pack $ show x), Right <$> await y ]
---       label x' y
-
 counter x = do
   div [ onClick ] [ text (T.pack $ show x) ]
   counter (x + 1)
-
-testReplica = do
-  runReplica $ local $ \e -> do
-    div [ style [("color", "red")], onClick ] [ text "Synchron" ]
-    div [ style [("color", "green")], onClick ] [ text "Synchron2" ]
-    div [ style [("color", "blue")] ] [ text "Synchron3" ]
 
 inputOnEnter p v = do
   e <- input [ autofocus True, placeholder p, value v, Left <$> onInput, Right <$> onKeyDown ]
@@ -203,26 +59,16 @@ inputOnEnter p v = do
       then pure v
       else inputOnEnter p v
 
-addition = do
-  a <- inputOnEnter "X" ""
-  button [ onClick ] [ text "Next" ]
-  b <- inputOnEnter "Y" ""
-  button [ onClick ] [ text "Add" ]
-  div [ onClick ] [ text ("Result :" <> T.pack (show (read (T.unpack a) + read (T.unpack b)))) ]
-  addition
+inputOnInput p v = do
+  e <- input [ autofocus True, placeholder p, value v, onInput]
+  pure . targetValue . target $ e
 
-additions2 = andd (addition, addition)
+inputPassword p v = do
+  e <- input [ autofocus True, placeholder p, type_ "password", value v, onInput]
+  pure . targetValue . target $ e
 
-additions = pool $ \p -> do
-  spawn p $ div [ key "1" ] [ addition ]
-  spawn p $ div [ key "2" ] [ addition ]
-  spawn p $ div [ key "3" ] [ addition ]
-  spawn p $ div [ key "4" ] [ addition ]
-  spawn p $ div [ key "5" ] [ addition ]
-  Syn.forever
-  
---------------------------------------------------------------------------------
 
+ 
 shared :: Syn () ()
 shared = local $ \end -> local $ \st -> pool $ \p -> do
   spawn p (set st end)
@@ -241,70 +87,6 @@ shared = local $ \end -> local $ \st -> pool $ \p -> do
       async (print a)
       get st
 
-background = div
-  [ style [("backgroundColor", "#d4dde0"), ("width", "100%"), ("height", "100%"), ("padding", "0px"), ("margin", "0px")] ]
-  [ div [ style [("fontFamily", "monospace"), ("fontWeight", "500"), ("color", "#4b5060")] ] [ text "Hello" ] ]
-
---------------------------------------------------------------------------------
-
-testDist = do
-    ctx <- run (NodeId 1) (counter 0)
-
-    let remoteCounter = newTrail ctx
-
-    runReplica $ local $ \e -> do
-      div [] [ remote remoteCounter, remote remoteCounter ]
-
-  where
-    counter x = do
-      div [ onClick ] [ text (T.pack $ show x) ]
-      counter (x + 1)
-
---------------------------------------------------------------------------------
-
-data Todo = Todo
-  { task :: T.Text
-  , done :: Bool
-  } deriving Show
-
-l = Left
-r = Right
-
-ll = l . l
-lr = l . r
-rl = r . l
-rr = r . r
-
-data Filter = Active | Inactive | All deriving Show
-
-todo' v x p = do
-  t <- inputOnEnter "What needs to be done?" ""
-  spawn p (div [ key (T.pack $ show x) ] [ todo v (Todo t False) ])
-  todo' v (x + 1) p
-
-todo v t = loop v (go t)
-  where
-    go t = stream $ \s -> if visible (s, done t)
-      then do
-        r <- div []
-          [ ll <$> input [ type_ "checkbox", checked (done t), onChange ]
-          , rl <$> ttext (task t)
-          , rr <$> span [ onClick ] [ text "     x" ]
-          , span [] [ text (T.pack $ show (done t)) ]
-          ]
-        case r of
-          Right (Left task') -> do
-            pure $ Right $ go (t { task = task' })
-          Left (Left _) -> do
-            pure $ Right $ go (t { done = not (done t) })
-          Right (Right _) -> pure $ Left ()
-      else empty
-
-    visible (Last All, _) = True
-    visible (Last Active, True) = True
-    visible (Last Inactive, False) = True
-    visible _ = False
-
 ttext t = do
   span [ onDoubleClick ] [ text t ]
   inputOnEnter "" t
@@ -315,91 +97,44 @@ ttext t = do
 -- a bit like parent widget keep control of shared state in Flutter.
 --
 -- Okay, so if I don't use spawn, it means squential showing widgets, one after another.
-inputName = var "" $ \v -> pool $ \p -> do
-  spawn p (inputName' v "") --spawn is a bit like nailing a varying painting on the wall. It decides sequence I guess. Or is it just about event pool?
-  spawn p (reactText v)
-  Syn.forever -- or Syn will end instantly. If there is a Syn here, the whole Syn will end with its branching logic.
-  where
-    inputName' v s = go s
-      where 
-            go s = do
-              typing <- inputOnEnter "請輸入姓名" s
-              putVar v typing
-              go typing 
-    reactText v = loop v $ stream $ \s -> do
-      text (T.pack $ show s)
-
-todos = var (Last All) $ \v -> pool $ \p -> do
-  spawn p (filters v)
-  spawn p (reactText v)
-  todo' v 0 p
-  where
-    filters v = do
-      r <- div []
-        [ All <$ div [ onClick ] [ text "All" ]
-        , Active <$ div [ onClick ] [ text "Active" ]
-        , Inactive <$ div [ onClick ] [ text "Inactive" ]
-        ]
-      putVar v (Last r)
-      filters v
-
-manytodos = div [] (take 100 $ repeat todos)
-
---------------------------------------------------------------------------------
-
-todosingle = var (Last Inactive) $ \v -> todo v (Todo "asd" False)
-
-todosingle2 = do
-  r <- div []
-    [ ll <$> input [ type_ "checkbox", checked (done t), onChange ]
-    , rl <$> ttext (task t)
-    , rr <$> span [ onClick ] [ text "     x" ]
-    , span [] [ text (T.pack $ show (done t)) ]
-    ]
+-- spawn is a bit like nailing a varying painting on the wall. It decides sequence I guess. Or is it just about event pool?
+--  Syn.forever or Syn will end instantly. If there is a Syn here, the whole Syn will end with its branching logic.
+--  spawn widget shows later than plain widget
+--  spawns in andd shows horiztonal and reversed.
+--  So no vertical for spawns.
+--  But in todos, spawn div then vertical.
+vert xs = div [] (map (\x -> div [] [x]) xs)
+signUp = var "" $ \name -> var "" $ \pwd -> var "" $ \cfm -> pool $ \p -> do
+  spawn p (div [] [inputName name])
+  spawn p (div [] [validateName name])
+  spawn p (div [] [inputPwd pwd])
+  spawn p (div [] [validatePwd pwd])
+  spawn p (div [] [inputCfm cfm])
+  spawn p (div [] [validateCfm pwd cfm])
+  -- vert [spawn p (validateName name), spawn p (inputName name)]
   Syn.forever
   where
-    t = Todo "test" False
-
---------------------------------------------------------------------------------
-
-reactText v = loop v $ stream $ \s -> do
-  div [ onClick ] [ text "CLICK ME!!!" ]
-  counter 0
-  text (T.pack $ show s)
-
-remoteContext nid v = Syn.run nid (reactText v)
-
-remoteText nid v = do
-  ctx <- remoteContext nid v
-  newTrail ctx
-
-testRemote = do
-  runReplica $ pool $ \p -> var (Last "") $ \v -> do
-    spawn p (go v)
-    spawn p (remote $ remoteText 1 v)
-    spawn p (remote $ remoteText 2 v)
-    spawn p (remote $ remoteText 3 v)
-    spawn p (remote $ remoteText 4 v)
-    spawn p (remote $ remoteText 5 v)
-    Syn.forever
-  where
-    go v = do
-      s <- div [] [ inputOnEnter "" "" ]
-      putVar v (Last s)
-      go v
-
---------------------------------------------------------------------------------
-
-trail p = runReplica $ svg
-  [ width "1000", height "1000", version "1.1", xmlns ]
-  [ synSvg' p ]
-
-
---------------------------------------------------------------------------------
-
-room msgs = do
-  msg <- div [] $ mconcat
-    [ [ inputOnEnter "" "" ]
-    , flip map msgs $ \msg -> div [] [ text msg ]
-    ]
-  room (msg:msgs)
+    inputName v = go mempty
+      where 
+            go s = do
+              s <- inputOnInput "請輸入姓名" s
+              putVar v s
+              go s 
+    validateName v = loop v $ stream $ \s -> do
+      text (T.pack $ show s)
+    inputPwd v = go mempty
+      where go s = do
+              s <- inputPassword "請輸入密碼" s 
+              putVar v s
+              go s
+    validatePwd v = loop v $ stream $ \s -> do
+      text (T.pack $ show s)
+    inputCfm v = go mempty
+      where go s = do
+              s <- inputPassword "請輸入密碼" s 
+              putVar v s
+              go s
+    validateCfm vPwd vCfm = loop vPwd $ stream $ \sPwd -> loop vCfm $ stream $ \sCfm -> do
+      text (T.pack $ show (sPwd == sCfm))
+ 
+main = runReplica signUp 
