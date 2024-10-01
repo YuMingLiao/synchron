c9a2349eab6ee83f167e59e1e2d104d85aa9efd5
YuMingLiao <dycan0716@gmail.com>
2024-10-01 10:12:08 +0000

    hpack to create concur-control.cabal and haskell-flake a flake.

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  inputs = rec {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      haskell-flake,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
+
+      perSystem =
+        {
+          self',
+          pkgs,
+          config,
+          ...
+        }:
+        {
+          haskellProjects.ghc965 = {
+            basePackages = pkgs.haskell.packages.ghc965;
+           };
+
+          haskellProjects.default = {
+            # The base package set representing a specific GHC version.
+            # By default, this is pkgs.haskellPackages.
+            # You may also create your own. See https://community.flake.parts/haskell-flake/package-set
+            basePackages = config.haskellProjects.ghc965.outputs.finalPackages;
+
+            packages = {
+              # aeson.source = "1.5.0.0";      # Override aeson to a custom version from Hackage
+              # shower.source = inputs.shower; # Override shower to a custom source path
+            };
+            settings = {
+              #  aeson = {
+              #    check = false;
+              #  };
+              #  relude = {
+              #    haddock = false;
+              #    broken = false;
+              #  };
+            };
+
+            devShell = {
+              # Enabled by default
+              # enable = true;
+
+              # Programs you want to make available in the shell.
+              # Default programs can be disabled by setting to 'null'
+              # tools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
+
+              # Check that haskell-language-server works
+              # hlsCheck.enable = true; # Requires sandbox to be disabled
+            };
+          };
+
+          # haskell-flake doesn't set the default package, but you can do it here.
+          # packages.default = config.haskellProjects.ghc948.outputs.finalPackages.lattices;
+          packages.default = self'.packages.concur-control; 
+        };
+    };
+}
